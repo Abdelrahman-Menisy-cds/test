8a7cea931da1b02b0c404efce2d49948db49049d
Menisy <a.mansy@cdsegypt.com>
2026-01-02 16:07:53 +0200

    update

--- addons/cds_sale_internal_transfer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2021 CDS Solutions SRL. (http://cdsegypt.com)
-#
-# Maintainer: Eng.Ramadan Khalil (<ramadan.khalil@cdsegypt.com>),Abdelrahman Menisy (<a.mansy@cdsegypt.com>) ,
-# It is forbidden to publish, distribute, sublicense, or sell copies
-# of the Software or modified copies of the Software.
-
-from . import sale_order
